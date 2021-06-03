2cb0e79f7ca4e9f3c084b8762af8912854e09a2b
Grace Go <grace@Graces-Macbook.local>
2021-06-02 22:02:23 -0500

    added description func

--- bot.py
+++ bot.py
@@ -2,10 +2,15 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
+@bot.command(name="description")
+async def description(ctx):
+    await ctx.send("If you want to get a recommendation randomly, type \"!random\". \n ")
+
 @bot.command(name="idea")
 async def idea(ctx):
     await ctx.send("Ideas are hard")
 
+# make sure to create a token file (in real life use env variables)
 with open("BOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
     print("Token file read")
